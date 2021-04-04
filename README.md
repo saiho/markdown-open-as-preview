ad6181f0f4a636b31008d48504bcbb84e4ee005b
saiho <16374397+saiho@users.noreply.github.com>
2021-04-04 14:44:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# **Markdown open as preview only** extension for Visual Studio Code
+
+This is a very simple extension. If enabled, whenever a Markdown file is opened you will initially see the preview instead of the editor.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release.
---
+++ src/extension.ts
@@ -0,0 +1,24 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    // Register when a new document is opened
+    vscode.workspace.onDidOpenTextDocument((doc: vscode.TextDocument) => {
+        openMarkdowPreview(doc);
+    });
+
+    // When the extension is activated and there is a document already open
+    openMarkdowPreview(vscode.window.activeTextEditor?.document);
+}
+
+export function deactivate() {}
+
+function openMarkdowPreview(doc: vscode.TextDocument | undefined) {
+    if (doc && doc.languageId === "markdown") {
+        vscode.commands.executeCommand("markdown.showPreview").then(() => {
+            vscode.window.showTextDocument(doc).then(() => {
+                vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+            });
+        });
+    }
+}
