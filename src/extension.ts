8376003e66d71d997de15c35279e8ddb934c0d0d
saiho <16374397+saiho@users.noreply.github.com>
2021-04-04 23:54:15 +0200

    Fixes

--- src/extension.ts
+++ src/extension.ts
@@ -11,14 +11,26 @@ export function activate(context: vscode.ExtensionContext) {
     openMarkdowPreview(vscode.window.activeTextEditor?.document);
 }
 
-export function deactivate() {}
+export function deactivate() { }
 
 function openMarkdowPreview(doc: vscode.TextDocument | undefined) {
-    if (doc && doc.languageId === "markdown" && !doc.isDirty && !doc.isUntitled) {
+    if (doc && doc.languageId === "markdown" && !doc.isDirty && !doc.isUntitled && doc.uri.scheme !== "git" && doc.uri.scheme !== "vscode" && doc.uri.scheme !== "output") {
         vscode.commands.executeCommand("markdown.showPreview").then(() => {
-            vscode.window.showTextDocument(doc).then(() => {
-                vscode.commands.executeCommand("workbench.action.closeActiveEditor");
-            });
+            if (vscode.window.activeTextEditor) {
+                // Be sure the preview finishes loading
+                vscode.window.showTextDocument(vscode.window.activeTextEditor.document, { preview: false }).then(() => {
+                    // Activate now the editor in order to close it
+                    vscode.commands.executeCommand("markdown.showSource").then(() => {
+                        if (vscode.window.activeTextEditor) {
+                            // Be sure the preview finishes loading
+                            vscode.window.showTextDocument(vscode.window.activeTextEditor.document, { preview: false }).then(() => {
+                                // Close the editor
+                                vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+                            });
+                        }
+                    });
+                });
+            }
         });
     }
 }
